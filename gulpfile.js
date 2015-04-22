164f4fe7e8c2b52c7b6bc10482fd95f074958466
Steven John Miller <steven.jmiller@gmail.com>
2015-04-22 21:22:53 +1000

    initial react stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp');
 var path = require('path');
 var less = require('gulp-less');
+var react = require('gulp-react');
+var connect = require('gulp-connect');
 
 gulp.task('clean', function(cb) {
   // You can use multiple globbing patterns as you would with `gulp.src`
   del(['build'], cb);
 });
-
-
 
 gulp.task('less', function () {
   return gulp.src('./less/**/*.less')
@@ -16,3 +16,27 @@ gulp.task('less', function () {
     }))
     .pipe(gulp.dest('./public/css'));
 });
+
+gulp.task('react', function () {
+  gulp.src('src/js/*.jsx')
+    .pipe(react())
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'src',
+    livereload: true
+  });
+});
+
+gulp.task('html', function () {
+  gulp.src('./src/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./src/*.html'], ['html']);
+});
+
+gulp.task('default', ['connect', 'watch']);
