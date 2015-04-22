6a6c1b21764d322b54c9f4e04d499e29fc80f618
Steven John Miller <steven.jmiller@gmail.com>
2015-04-22 21:48:37 +1000

    Get Example working via build

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,31 @@
+var del = require('del');
 var gulp = require('gulp');
 var path = require('path');
+var open = require('gulp-open');
 var less = require('gulp-less');
 var react = require('gulp-react');
+var bower = require('gulp-bower');
 var connect = require('gulp-connect');
+var runSequence = require('run-sequence');
 
 gulp.task('clean', function(cb) {
-  // You can use multiple globbing patterns as you would with `gulp.src`
-  del(['build'], cb);
+  del(['./dist'], cb);
+});
+
+gulp.task('bower', function() {
+  return bower()
+    .pipe(gulp.dest('dist/lib/'))
 });
 
 gulp.task('less', function () {
-  return gulp.src('./less/**/*.less')
+  gulp.src('./less/**/*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('react', function () {
+gulp.task('jsx', function () {
   gulp.src('src/js/*.jsx')
     .pipe(react())
     .pipe(gulp.dest('dist/js'));
@@ -25,18 +33,29 @@ gulp.task('react', function () {
 
 gulp.task('connect', function() {
   connect.server({
-    root: 'src',
+    root: 'dist',
     livereload: true
   });
 });
 
 gulp.task('html', function () {
   gulp.src('./src/*.html')
+    .pipe(gulp.dest('./dist'))
     .pipe(connect.reload());
+});
+
+gulp.task('open', function(){
+  gulp.src('./dist/index.html')
+    .pipe(open('', {
+      url: 'http://localhost:8080'
+    }));
 });
 
 gulp.task('watch', function () {
   gulp.watch(['./src/*.html'], ['html']);
+  gulp.watch(['./src/js/*.jsx'], ['jsx']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', function(callback) {
+  runSequence('clean', 'bower', 'html', 'jsx', 'connect', 'open', 'watch', callback);
+});
